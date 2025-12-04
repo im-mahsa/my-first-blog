ddfd17a19e489e4a01e1a558cd2e838255512ada
mashkoori <mashkoori@gmail.com>
2025-12-04 22:01:03 +0330

    Added view and template for detailed blog post as well as CSS for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,11 +118,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "blog/static",
-]
+#STATICFILES_DIRS = [
+#    BASE_DIR / "blog/static",
+#]
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
